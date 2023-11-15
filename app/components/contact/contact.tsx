ee3fbb27b1f4692fbf78512a02f18004e33c7cfa
Immanuel Shikanda <shikandaimmanuel5@gmail.com>
2023-11-15 14:50:17 +0300

    Footer content Update and Contact Component Create

---
+++ app/components/contact/contact.tsx
@@ -0,0 +1,13 @@
+import Nav from "../nav/nav";
+
+export default function Contact() {
+  return (
+    <>
+      <section>
+        <form action="">
+          Contact Form
+        </form>
+      </section>
+    </>
+  );
+}
--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -3,9 +3,19 @@ import Nav from "../nav/nav";
 export default function Footer() {
   return (
     <>
-      <header>
-        <Nav />
-      </header>
+      <section>
+        <div>
+          <a href="">Facebook</a>
+          <a href="">X</a>
+          <a href="">Instagram</a>
+        </div>
+        <div>
+          <Nav/>
+        </div>
+        <div>
+          Newsletter Subscription
+        </div>
+      </section>
     </>
   );
 }
