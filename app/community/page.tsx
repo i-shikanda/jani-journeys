2bc2ee9e6412424be049e364a2954dbad63f8617
Immanuel Shikanda <shikandaimmanuel5@gmail.com>
2023-11-15 14:26:37 +0300

    Community Information Update

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -1,8 +1,12 @@
 export default function Community() {
   return (
-    <section>
-      <div>User Generated Feed, with likes,comments and share</div>
-      <div>Testimonials</div>
-    </section>
+    <>
+      <section>
+        <div>User Generated Feed, with likes,comments and share</div>
+      </section>
+      <section>
+        <div>Testimonials Carousel</div>
+      </section>
+    </>
   );
 }
