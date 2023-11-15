c3f583aec9b5aab9d36818f53f74629b288ece37
Immanuel Shikanda <shikandaimmanuel5@gmail.com>
2023-11-15 14:07:43 +0300

    Destinations Info Update

--- app/destinations/page.tsx
+++ app/destinations/page.tsx
@@ -1,14 +1,62 @@
+import Image from "next/image";
+import Nav from "../components/nav/nav";
 export default function Destinations() {
-  // const destinationInfo = (
-  //     // Call the destination floating more info
-  // );
+  const destinations = [
+    {
+      destinationName: "Utali Beach",
+      image: "/utali.jpg",
+      destinationDescription: "A Breaf Description of Utali",
+    },
+    {
+      destinationName: "Utali kBeach",
+      image: "/utali.jpg",
+      destinationDescription: "A Breaf Description of Utali",
+    },
+    {
+      destinationName: "Utalif Beach",
+      image: "/utali.jpg",
+      destinationDescription: "A Breaf Description of Utali",
+    },
+    {
+      destinationName: "Utalji Beach",
+      image: "/utali.jpg",
+      destinationDescription: "A Breaf Description of Utali",
+    },
+    {
+      destinationName: "Utadli Beach",
+      image: "/utali.jpg",
+      destinationDescription: "A Breaf Description of Utali",
+    },
+    {
+      destinationName: "Utdali Beach",
+      image: "/utali.jpg",
+      destinationDescription: "A Breaf Description of Utali",
+    },
+  ];
+
   return (
     <section>
-      <div id="interactivemap"></div>
-      <div id="filters">
-        {/* <div id="destination pages" onClick={destinationInfo}>
-                
-                </div> */}
+      <header>
+        <Nav />
+      </header>
+      <div id="interactivemap">Map</div>
+      <div className="grid grid-cols-4">
+        {destinations.map((utali, desti) => {
+          return (
+            <div key={desti}>
+              <Image
+                src={utali.image}
+                alt="Destination"
+                width={100}
+                height={100}
+              />
+              <div>
+                <h1>{utali.destinationName}</h1>
+                <p>{utali.destinationDescription}</p>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
