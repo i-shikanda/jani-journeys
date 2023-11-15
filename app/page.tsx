be72ca4526f8a99a0477eac96daeea1a9dbb606c
Immanuel Shikanda <shikandaimmanuel5@gmail.com>
2023-11-15 13:44:31 +0300

    Home Page Content Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,134 @@
 import Footer from "./components/footer/footer";
 import Nav from "./components/nav/nav";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <header>
-        <Nav />
-      </header>
-      <section id="hero">
-        <h1>Environment Partners</h1>
-        <div id="calltoaction">
-          <p>Text Text Text</p>
-          <a href="">
-            <button>Plan Your Green Journey</button>
-          </a>
+      <section id="hero" className="relative">
+        <div className="absolute w-full h-full -z-10">
+          <div className="relative h-full w-full">
+            <Image
+              objectFit="cover"
+              objectPosition="center"
+              alt="Background"
+              layout="fill"
+              src="/home.jpg"
+              quality={100}
+            />
+          </div>
+        </div>
+        <header>
+          <Nav />
+        </header>
+        <div className="flex justify-between space-x-4">
+          <div id="calltoaction">
+            <h1 className="font-bold text-2xl uppercase">
+              Embark on <br /> Eco-Friendly Journeys with <br />{" "}
+              <span className="text-green-600">Jani Journey</span> <br /> Your
+              Environment Partners.
+            </h1>
+            <a href="">
+              <button className="px-4 py-2 rounded-sm bg-green-00 button font-medium">
+                Plan Your Green Journey
+              </button>
+            </a>
+          </div>
+          <div>Social Media Icons</div>
         </div>
       </section>
       <section id="feautereddestinations">
         <div className="grid grid-cols-4">
-          {/* Display a carousel or grid of destinations with high-quality images and concise descriptions. */}
+          <a href="">
+            <div>
+              <Image
+                src="/destination"
+                alt="Destination"
+                width={100}
+                height={100}
+              />
+              <div>
+                <h1>Destination Name</h1>
+                <p>Unique Eco-Friendly Feutures</p>
+              </div>
+            </div>
+          </a>
+          <a href="">
+            <div>
+              <Image
+                src="/destination"
+                alt="Destination"
+                width={100}
+                height={100}
+              />
+              <div>
+                <h1>Destination Name</h1>
+                <p>Unique Eco-Friendly Feutures</p>
+              </div>
+            </div>
+          </a>
+          <a href="">
+            <div>
+              <Image
+                src="/destination"
+                alt="Destination"
+                width={100}
+                height={100}
+              />
+              <div>
+                <h1>Destination Name</h1>
+                <p>Unique Eco-Friendly Feutures</p>
+              </div>
+            </div>
+          </a>
+          <a href="">
+            <div>
+              <Image
+                src="/destination"
+                alt="Destination"
+                width={100}
+                height={100}
+              />
+              <div>
+                <h1>Destination Name</h1>
+                <p>Unique Eco-Friendly Feutures</p>
+              </div>
+            </div>
+          </a>
         </div>
         <div>
-          {/* Include icons or symbols indicating eco-friendly practices like wildlife conservation, renewable energy, or waste reduction. */}
+          <div>Eco-Certified Accomodations</div>
+          <div>Renewable Energy Usage</div>
+          <div>Local Community Engagement</div>
         </div>
       </section>
       <section id="greentraveltips">
-        <div id="engaging graphics"></div>
+        <div id="engaging graphics">
+          <div>
+            <Image src="/image" width={100} height={100} alt="Eco Tip" />
+            <div>
+              <h1>Reusable Water Bottle</h1>
+            </div>{" "}
+          </div>
+          <div>
+            <Image src="/image" width={100} height={100} alt="Eco Tip" />
+            <div>
+              <h1>Reusable Water Bottle</h1>
+            </div>
+          </div>
+          <div>
+            <Image src="/image" width={100} height={100} alt="Eco Tip" />
+            <div>
+              <h1>Reusable Water Bottle</h1>
+            </div>
+          </div>
+          <div>
+            <Image src="/image" width={100} height={100} alt="Eco Tip" />
+            <div>
+              <h1>Reusable Water Bottle</h1>
+            </div>{" "}
+          </div>
+        </div>
       </section>
       <footer>
         <Footer />
