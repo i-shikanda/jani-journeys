a78615d9a9e523feffda9ea8c10b4de323cc0b59
Immanuel Shikanda <shikandaimmanuel5@gmail.com>
2023-11-15 14:42:24 +0300

    Partners content updated

--- app/partnership/page.tsx
+++ app/partnership/page.tsx
@@ -1,21 +1,72 @@
+import Image from "next/image";
 import Footer from "../components/footer/footer";
 import Nav from "../components/nav/nav";
 
 export default function Partnership() {
+  const partners = [
+    {
+      image: "/Logo",
+      name: "Partner",
+      link: "partners.com",
+    },
+    {
+      image: "/Logo",
+      name: "Partner",
+      link: "partners.com",
+    },
+    {
+      image: "/Logo",
+      name: "Partner",
+      link: "partners.com",
+    },
+    {
+      image: "/Logo",
+      name: "Partner",
+      link: "partners.com",
+    },
+  ];
   return (
     <>
-      <header>
-        <Nav />
-      </header>
       <section>
-        <div>Feutured Partners, with their social media links</div>
+        <header>
+          <Nav />
+        </header>
+        <div>Our Patners towards a greener community</div>
+        <div className="grid grid-cols-4">
+          {partners.map((part, ners) => {
+            return (
+              <div key={ners}>
+                <a href={part.link}>
+                  <div>
+                    <Image
+                      src={part.image}
+                      width={100}
+                      height={100}
+                      alt="Partner"
+                    />
+                    <h1>{part.name}</h1>
+                  </div>
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+      <section className="flex justify-between items-center">
         <div>
           <a href="">
             <button>Become a Partner</button>
           </a>
         </div>
+        <div>
+          <h1>Steps to Partnership</h1>
+          <p>
+            1. Blabla Bla <br />
+            2. B;aanan <br />
+            3. Belle <br />
+          </p>
+        </div>
       </section>
-      <section>steps to Partnership</section>
       <footer>
         <Footer />
       </footer>
