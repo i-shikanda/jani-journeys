86af16ba55131c91cdbbc1f145558dbfc27a6b0b
Immanuel Shikanda <shikandaimmanuel5@gmail.com>
2023-11-15 14:27:57 +0300

    Updated Nav into Community

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -1,7 +1,12 @@
+import Nav from "../components/nav/nav";
+
 export default function Community() {
   return (
     <>
       <section>
+        <header>
+          <Nav/>
+        </header>
         <div>User Generated Feed, with likes,comments and share</div>
       </section>
       <section>
